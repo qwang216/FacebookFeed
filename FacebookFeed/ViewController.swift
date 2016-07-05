fa10287c53e3f56a44bc760c1635a9833382967c
Jason Wang <qwang216@gmail.com>
2016-07-05 02:16:17 -0400

    inital commit

---
+++ FacebookFeed/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FacebookFeed
+//
+//  Created by Jason Wang on 7/5/16.
+//  Copyright © 2016 Jason Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
